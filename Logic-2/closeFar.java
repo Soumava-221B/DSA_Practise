698402a30fa9a72528dba60feaa1dc4bb8b5be5f
Soumava Das <soumavadas06@gmail.com>
2024-03-10 17:35:36 +0000

    .

---
+++ Logic-2/closeFar.java
@@ -0,0 +1,13 @@
+/*
+Given three ints, a b c, return true if one of b or c is "close" (differing from a by at most 1), while the other is "far", differing from both other values by 2 or more. Note: Math.abs(num) computes the absolute value of a number.
+
+closeFar(1, 2, 10) → true
+closeFar(1, 2, 3) → false
+closeFar(4, 1, 3) → true
+*/
+
+public boolean closeFar(int a, int b, int c) {
+ boolean bIsClose = Math.abs(b - a) <= 1 && Math.abs(c - a) >= 2 && Math.abs(c - b) >= 2;
+    boolean cIsClose = Math.abs(c - a) <= 1 && Math.abs(b - a) >= 2 && Math.abs(b - c) >= 2;
+    return bIsClose || cIsClose;
+}
